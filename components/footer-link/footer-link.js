e2f97378e1cded7f49a4fd85484ef5d02309ca20
bhyun-kim <kimbh.mail@gmail.com>
2024-06-06 14:51:06 -0500

    Add foot-link-component

---
+++ components/footer-link/footer-link.js
@@ -0,0 +1,29 @@
+const template = document.createElement("template");
+template.innerHTML = `
+  <link rel="stylesheet" href="./components/footer-link/footer-link.css">
+  <ul class="footer-link">
+    <li>
+      <a id="link" href="#">FAQ</a>
+    </li>
+  </ul>
+`;
+
+class FooterLinkComponent extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: "open" });
+    shadow.appendChild(template.content.cloneNode(true));
+  }
+
+  static get observedAttributes() {
+    return ["href"];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === "href") {
+      this.shadowRoot.getElementById("link").setAttribute("href", newValue);
+    }
+  }
+}
+
+window.customElements.define("footer-link-component", FooterLinkComponent);
