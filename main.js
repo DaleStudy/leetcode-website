16c6943ef070680414e30e9485438b66218ab24d
sounmind <devleesche@gmail.com>
2024-06-08 12:14:37 -0400

    feat: add link button component

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import "./components/divider/divider.js";
 import "./components/footer-link/footer-link.js";
 import "./components/hero/hero.js";
+import "./components/link-button/link-button.js";
 import "./components/seo-meta-tag/seo-meta-tag.js";
