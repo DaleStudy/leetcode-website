18518518467cfffa0ab8224648bd06f48936d75e
SamTheKorean <bumsuyi@gmail.com>
2024-06-11 23:34:28 +0900

    feat : completely integrated link-button component into header component

--- components/header/header.js
+++ components/header/header.js
@@ -1,15 +1,18 @@
+import '../link-button/link-button.js';
+
 const template = document.createElement('template');
 template.innerHTML = `
   <link rel="stylesheet" href="./components/header/header.css">
   <header>
     <div class="container">
-    <a href="#">
-      <img src="images/logo.png" alt="logo" />
-      <span>달레 스터디</span>
-    </a>
-    </div>
-    <div>
-      
+      <a href="#">
+        <img src="images/logo.png" alt="logo" />
+        <span>달레 스터디</span>
+      </a>
+      <div class="buttons-container">
+        <link-button href="#join-instruction-container" size="large" variant="primary">참여방법 안내</link-button>
+        <link-button href="https://discord.gg/43UkheRV" size="large" variant="secondary">디스코드 참여하기</link-button>
+      </div>
     </div>
   </header>
 `;
--- main.js
+++ main.js
@@ -3,3 +3,4 @@ import "./components/footer-link/footer-link.js";
 import "./components/hero/hero.js";
 import "./components/link-button/link-button.js";
 import "./components/seo-meta-tag/seo-meta-tag.js";
+import "./components/header/header.js"
