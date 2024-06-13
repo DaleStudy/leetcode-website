32ec7fab6222f397ffabf8360c35a51dae185a0d
SamTheKorean <bumsuyi@gmail.com>
2024-06-13 22:53:44 +0900

    fix : apply feedback in pull request

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import "./components/divider/divider.js";
 import "./components/footer-link/footer-link.js";
+import "./components/header.js"
 import "./components/hero/hero.js";
 import "./components/link-button/link-button.js";
 import "./components/seo-meta-tag/seo-meta-tag.js";
-import "./components/header/header.js"
+
