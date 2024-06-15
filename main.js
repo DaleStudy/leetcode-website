72e4dcca2ff396cc06dc8d964387737ffc65695e
bhyun-kim <kimbh.mail@gmail.com>
2024-06-14 23:36:32 -0500

    Remove footer-icon folder

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import "./components/divider/divider.js";
 import "./components/footer-link/footer-link.js";
-import "./components/footer-icon/footer-icon.js";
+import "./components/footer-icon.js";
 import "./components/header.js";
 import "./components/hero/hero.js";
 import "./components/link-button/link-button.js";
