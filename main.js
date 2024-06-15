f00d3375de07703842e9519e3fbb2b08cb76a29a
hwigyeompark <phg5590@gmail.com>
2024-06-15 18:05:42 -0400

    refactor: modify component path in main.js

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ import "./components/divider/divider.js";
 import "./components/footer-link/footer-link.js";
 import "./components/hero/hero.js";
 import "./components/link-button/link-button.js";
-import "./components/review-item/review-item.js";
+import "./components/review-item.js";
 import "./components/seo-meta-tag/seo-meta-tag.js";
