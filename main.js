e86da53616cf39730760a13846041f950712fd7e
SamTheKorean <bumsuyi@gmail.com>
2024-06-24 22:18:27 +0900

    fix : adjust to new component writting styles

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import "./components/footer-link/footer-link.js";
 import "./components/header.js";
-import "./components/hero/hero.js";
+import "./components/hero.js";
 import "./components/button-link.js";
 import "./components/review-item/review-item.js";
 import "./components/seo-meta-tag/seo-meta-tag.js";
