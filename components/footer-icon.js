21f7d2e924f2bcf000402d6c8e57aad8282e3e6d
bhyun-kim <kimbh.mail@gmail.com>
2024-06-25 19:28:33 -0500

    Add footer-icon

---
+++ components/footer-icon.js
@@ -0,0 +1,107 @@
+import { css, html } from "../html-css-utils.js";
+
+class FooterIcon extends HTMLElement {
+  constructor() {
+    super();
+
+    this.validateAttributes();
+    this.render();
+  }
+
+  validateAttributes() {
+    if (!this.hasAttribute("href")) {
+      throw new Error('The "href" attribute is required.');
+    }
+
+    if (!this.hasAttribute("type")) {
+      throw new Error('The "type" attribute is required.');
+    }
+  }
+
+  render() {
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = this.createCss() + this.createHtml();
+    this.updateIcon();
+  }
+
+  createCss() {
+    return css`
+      .footer-icon {
+        display: inline-block;
+      }
+      slot {
+      display: none;
+    `;
+    
+  }
+
+  createHtml() {
+    const type = this.getAttribute("type");
+    const href = this.getAttribute("href");
+
+    if (type == 'svg') {
+
+      const viewBox = this.getAttribute("viewBox");
+
+      return html`
+        <a href="${href}">
+          <svg
+           xmlns="http://www.w3.org/2000/svg"
+           width="24px"
+           height="24px"
+           viewBox="${viewBox}"
+          >
+            <path></path>
+          </svg>
+        </a>
+        <slot></slot>
+      `;
+    }
+    else if (type == 'img'){
+      return html`
+        <a href="${href}">
+          <img 
+           alt="icon"
+           width="24px"
+           height="24px"
+          >
+          </img>
+        </a>
+        <slot></slot>
+      `;
+    }
+    else {
+      throw new Error('The "type" attribute must be either "svg" or "img".');
+    }
+
+  }
+
+  updateIcon() {
+    const type = this.getAttribute("type");
+
+    if (type == 'svg') {
+      this.updateSvgPath();
+    }
+    else if (type == 'img'){
+      this.updateImgPath();
+    }
+  }
+
+  updateSvgPath() {
+    const path = this.shadowRoot.querySelector('path');
+    const slot = this.shadowRoot.querySelector('slot');
+    const d = slot.assignedNodes()[0].textContent.trim();
+
+    path.setAttribute('d', d);
+  }
+
+  updateImgPath() {
+    const img = this.shadowRoot.querySelector("img");
+    const slot = this.shadowRoot.querySelector('slot');
+    const src = slot.assignedNodes()[0].textContent.trim();
+
+    img.setAttribute("src", src);
+  }
+}
+
+customElements.define("footer-icon", FooterIcon);
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import "./components/footer-link/footer-link.js";
+import "./components/footer-icon.js";
 import "./components/header.js";
 import "./components/hero.js";
 import "./components/button-link.js";
