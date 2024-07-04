a406808c2ccdebffb41be2cb687d3c945d694b7e
sounmind <devleesche@gmail.com>
2024-07-04 19:28:44 -0400

    feat: icon-link-list, icon-link component
    - name changed: footer-icon -> icon-link

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import "./components/button-link.js";
-import "./components/footer-icon.js";
 import "./components/footer-link-list.js";
 import "./components/header.js";
 import "./components/hero.js";
+import "./components/icon-link-list.js";
 import "./components/image.js";
 import "./components/intro-section.js";
 import "./components/review-item.js";
